8bcdf1d98f4e3fdc8ebdec336617aa05f3ad618b
LordBobbert <113851769+LordBobbert@users.noreply.github.com>
2024-09-16 09:43:11 -0400

    Refactor ALLOWED_HOSTS to remove localhost from the list

--- backend/GymManager/settings.py
+++ backend/GymManager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY=os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['unifiedgymmanager-auc4gramc0b4fcgb.canadacentral-01.azurewebsites.net', 'localhost']
+ALLOWED_HOSTS = ['unifiedgymmanager-auc4gramc0b4fcgb.canadacentral-01.azurewebsites.net']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
