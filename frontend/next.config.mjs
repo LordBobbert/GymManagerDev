56d93f89869199eacaf7ba04ff8e1a26fc048867
LordBobbert <113851769+LordBobbert@users.noreply.github.com>
2024-09-16 10:54:49 -0400

    Refactor next.config.mjs to include environment variable for API base URL

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,16 @@
 // next.config.mjs
 
 export default {
-    // Enabling React's strict mode
-    reactStrictMode: true,
-    
-    // Specify file extensions for pages
-    pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-    
-    // Add any other custom configurations you need here
-  };
+  // Enabling React's strict mode
+  reactStrictMode: true,
   
+  // Specify file extensions for pages
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+
+  // Environment variables
+  env: {
+      NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+
+  // Add any other custom configurations you need here
+};
