50f9ff24b5eaaed376eeb8666fe6230dfd3feec1
LordBobbert <113851769+LordBobbert@users.noreply.github.com>
2024-09-23 08:51:11 -0400

    Refactor: Update CORS configuration in settings.py and set SameSite attribute to 'None'

--- backend/user_management/authentication.py
+++ backend/user_management/authentication.py
@@ -1,18 +1,22 @@
-# File: user_management/authentication.py
-
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.exceptions import AuthenticationFailed
+
 
 class CustomJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        # Try to get the token from the 'access_token' cookie
+        # First, try to get the token from the 'access_token' cookie
         raw_token = request.COOKIES.get('access_token')
 
         if raw_token is None:
             # Fallback to the Authorization header (Bearer token)
             header = self.get_header(request)
+            if header is None:  # No header is provided
+                return None  # No token found, authentication fails
             raw_token = self.get_raw_token(header)
 
         if raw_token is None:
-            return None
+            return None  # No token found, authentication fails
 
-        return self.get_user_and_token(raw_token)
+        validated_token = self.get_validated_token(raw_token)
+        return self.get_user(validated_token), validated_token
