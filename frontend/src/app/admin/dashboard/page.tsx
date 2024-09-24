3aefa2f5d8ab5961f1082e66eb3bd8cc21d73c82
LordBobbert <113851769+LordBobbert@users.noreply.github.com>
2024-09-24 15:56:30 -0400

    Refactor: Update admin dashboard page to remove layout component

--- frontend/src/app/admin/dashboard/page.tsx
+++ frontend/src/app/admin/dashboard/page.tsx
@@ -2,17 +2,16 @@
 
 import React from 'react';
 import PlaceholderCard from './components/PlaceholderCard';
-import AdminLayout from '../layout';
 
 const AdminDashboardPage = async () => {
   // Fetch any data you need for the page here, focusing on the rendering.
   // No need to fetch current user if middleware handles it
   return (
-    <AdminLayout>
+    <div>
       <PlaceholderCard title="Admin Home" />
       <PlaceholderCard title="Client Management" />
       <PlaceholderCard title="Session Management" />
-    </AdminLayout>
+    </div>
   );
 };
 
