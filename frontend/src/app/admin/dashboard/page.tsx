2c9f513230ce7c5837a1216a9bfe007227e8218c
LordBobbert <113851769+LordBobbert@users.noreply.github.com>
2024-09-24 15:55:34 -0400

    Refactor: Update admin dashboard page to use new layout component

--- frontend/src/app/admin/dashboard/page.tsx
+++ frontend/src/app/admin/dashboard/page.tsx
@@ -1,18 +1,18 @@
 // File: src/app/admin/dashboard/page.tsx
 
 import React from 'react';
-import AdminDashboardLayout from './layout';
 import PlaceholderCard from './components/PlaceholderCard';
+import AdminLayout from '../layout';
 
 const AdminDashboardPage = async () => {
   // Fetch any data you need for the page here, focusing on the rendering.
   // No need to fetch current user if middleware handles it
   return (
-    <AdminDashboardLayout>
+    <AdminLayout>
       <PlaceholderCard title="Admin Home" />
       <PlaceholderCard title="Client Management" />
       <PlaceholderCard title="Session Management" />
-    </AdminDashboardLayout>
+    </AdminLayout>
   );
 };
 
