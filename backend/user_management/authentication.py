643b836674a89e5c519e5ac59a11fbfbe40154a9
LordBobbert <113851769+LordBobbert@users.noreply.github.com>
2024-09-24 14:37:54 -0400

    Refactor: Update authentication.py to extract access token from cookies
    The code changes in authentication.py refactor the authentication process by updating the `authenticate` method. Instead of first trying to get the token from the 'access_token' cookie and then falling back to the Authorization header, the code now directly extracts the access token from the cookies. This change improves the clarity and efficiency of the authentication logic.

--- backend/user_management/authentication.py
+++ backend/user_management/authentication.py
@@ -1,22 +1,14 @@
+# user_management/authentication.py
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.exceptions import AuthenticationFailed
-
 
 class CustomJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        # First, try to get the token from the 'access_token' cookie
-        raw_token = request.COOKIES.get('access_token')
-
-        if raw_token is None:
-            # Fallback to the Authorization header (Bearer token)
-            header = self.get_header(request)
-            if header is None:  # No header is provided
-                return None  # No token found, authentication fails
-            raw_token = self.get_raw_token(header)
-
-        if raw_token is None:
-            return None  # No token found, authentication fails
-
-        validated_token = self.get_validated_token(raw_token)
+        # Extract the token from cookies
+        access_token = request.COOKIES.get('access_token')
+        
+        if access_token is None:
+            return None  # No access token in cookies
+        
+        # Validate and authenticate using the JWT token
+        validated_token = self.get_validated_token(access_token)
         return self.get_user(validated_token), validated_token
