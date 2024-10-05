386c02b96af60299b96fb047e4e5e7d36e072d59
LordBobbert <113851769+LordBobbert@users.noreply.github.com>
2024-10-04 22:04:55 -0400

    Refactor: Improve conditional rendering in ClientsPage component

--- frontend/src/app/admin/clients/page.tsx
+++ frontend/src/app/admin/clients/page.tsx
@@ -162,13 +162,12 @@ const ClientsPage = () => {
                               <TableCell>{session.date}</TableCell>
                               <TableCell>{session.session_type}</TableCell>
                               <TableCell>
-                                {session.trainer && 'user' in session.trainer ? (
+                                {session.trainer && typeof session.trainer === 'object' && 'user' in session.trainer ? (
                                   `${session.trainer.user.first_name} ${session.trainer.user.last_name}`
                                 ) : (
                                   "Trainer Not Assigned"
                                 )}
                               </TableCell>
-
                             </TableRow>
                           ))}
                         </TableBody>
