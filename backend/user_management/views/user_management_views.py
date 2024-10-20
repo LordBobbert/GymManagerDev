85aa05b17269fcdf0c9b63151579e254937b4f27
LordBobbert <113851769+LordBobbert@users.noreply.github.com>
2024-10-20 13:24:36 -0400

    Refactor: Update import statement for User model in user_management_views.py

--- backend/user_management/views/user_management_views.py
+++ backend/user_management/views/user_management_views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from user_management.models import User
-from serializers import UserSerializer
+from user_management.serializers import UserSerializer
 from .permissions import IsAdmin, IsTrainer  # Assuming you have custom permissions defined
 
 # Function-based view to handle listing users by role
